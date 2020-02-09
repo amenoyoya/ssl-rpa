3f9217b2e18676370255351fe57073529ea525d5
amenoyoya <mtyswe3@gmail.com>
2020-02-10 00:54:53 +0900

    🐛 fix: puppeteerがDocker環境で起動しないバグを修正

--- app/api/index.js
+++ app/api/index.js
@@ -26,23 +26,29 @@ const sakura = require('./sakura');
 router.post('/', async (req, res) => {
   const response = {};
 
-  await sakura.puppet({}, async page => {
-    if (req.body.login_user && req.body.login_passwd) {
-      // ログイン
-      const panel_url = await sakura.login(page, req.body.login_user, req.body.login_passwd);
-      response.login_result = sakura.getLoginStatus(panel_url);
-      // SSL申請
-      if (Array.isArray(req.body.ssl_target_domains)) {
-        response.ssl_results = await sakura.requestSslDomains(page, panel_url, req.body.ssl_target_domains);
-      }
-      // ロギング設定
-      if (typeof req.body.logging === 'object') {
-        response.logging_result = await sakura.changeLogging(
-          page, panel_url, req.body.logging.log, req.body.logging.rotation, req.body.logging.hosts
-        );
+  await sakura.puppet(
+    // Dockerで動かすとき用のオプション
+    process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD?
+      {executablePath: '/usr/bin/chromium-browser', args: ['--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox']}
+      : {args: ['--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox']},
+    async page => {
+      if (req.body.login_user && req.body.login_passwd) {
+        // ログイン
+        const panel_url = await sakura.login(page, req.body.login_user, req.body.login_passwd);
+        response.login_result = sakura.getLoginStatus(panel_url);
+        // SSL申請
+        if (Array.isArray(req.body.ssl_target_domains)) {
+          response.ssl_results = await sakura.requestSslDomains(page, panel_url, req.body.ssl_target_domains);
+        }
+        // ロギング設定
+        if (typeof req.body.logging === 'object') {
+          response.logging_result = await sakura.changeLogging(
+            page, panel_url, req.body.logging.log, req.body.logging.rotation, req.body.logging.hosts
+          );
+        }
       }
     }
-  });
+  );
   res.json(response);
 })
 
